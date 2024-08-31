12ba94b796b55fa875e01a4d273b486f162e1ec9
flavio <flaflagi@gmail.com>
2024-08-31 12:05:25 -0300

    Python alterado

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 from time import sleep
 
 # count from one to ten with a sleep of 1 second between each step
-print('Starting count ...')
+print('Starting count alterado...')
 
 for i in range(10):
     print(i)
     sleep(1)
 
-print('Done!')
+print('Done! alterado')
